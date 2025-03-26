4222ad8517c9524e2564abccbf9e360ca6843270
chocarro <chocarroo@gmail.com>
2025-03-26 16:52:16 +0100

    Cambios dia2

--- src/LecturaJSON.java
+++ src/LecturaJSON.java
@@ -1,3 +1,19 @@
-public class LecturaJSON {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+
+public class LecturaJSON {
+    public static List<Map<String, String>> leerJSON(String rutaArchivo) {
+        List<Map<String, String>> datos = new ArrayList<>();
+        try {
+            
+        } catch () {
+        }
+        return datos;
+    }
 }
--- src/LecturaXML.java
+++ src/LecturaXML.java
@@ -1,3 +1,41 @@
-public class LecturaXML {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class LecturaXML{
+    public static void LecturaXML{
+     try {
+            String linea;
+            String archivoEntrada;
+            BufferedReader reader = new BufferedReader(new FileReader( archivoEntrada));
+
+            while ((linea = reader.readLine()) != null) {
+                String[] valores = linea.split("\\s+");
+
+                if (valores.length > 1) {
+                    String jugador = valores[1];
+                    int censuradasEnLinea = 0;
+                    StringBuilder nuevaLinea = new StringBuilder();
+
+                    for (String archivo : valores) {
+                        if (archivo.length() > 1 && archivo.contains(archivo.toLowerCase())) {
+                        } else {
+                            nuevaLinea.append(archivo).append(" ");
+                        }
+                    }
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+   
